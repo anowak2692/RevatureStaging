2c251c17852b5c230b1abfebe1b368635c56c0a2
Alex Nowak <anowak2692@gmail.com>
2021-10-05 16:52:18 -0500

    Some additional comments and an additional test for invalid number of digits

--- Java/CreditCardValidator/src/CreditCardValidator.java
+++ Java/CreditCardValidator/src/CreditCardValidator.java
@@ -6,8 +6,9 @@ public class CreditCardValidator {
     }
 
     public static boolean validateCard(long cardNumber){
-        //Check the num of digits
-        if(cardNumber >= (Math.pow(10, 13)) && cardNumber < (Math.pow(10, 17))){
+        //Check the number of digits
+        if(cardNumber >= (Math.pow(10, 13)) && cardNumber < (Math.pow(10, 18))){
+            //Store the check digit for the end
             int checkDigit = (int) (cardNumber % 10);
             cardNumber /= 10;
             ArrayList<Integer> reverse = reverseDigitArray(cardNumber);
@@ -15,6 +16,7 @@ public class CreditCardValidator {
             //in the reversed array (0 index is the first position, so it is odd)
             for(int i = 0; i < reverse.size(); i += 2){
                 int doubledValue = reverse.get(i) * 2;
+                //Values greater than ten get their digits added to create a single-digit value
                 if(doubledValue >= 10){
                     int newValue = 1 + (doubledValue % 10);
                     reverse.set(i, newValue);
@@ -23,9 +25,12 @@ public class CreditCardValidator {
                     reverse.set(i, doubledValue);
                 }
             }
+            //Get the sum of the modified array by using the reduce method of Streams
             int sum = reverse.stream().reduce( (prev, current) -> prev + current).get();
+            //Check to see if 10 minus the last digit of the sum is equal to the check digit we saved before
             return (10 - (sum % 10)) == checkDigit;
         }
+        //Return false if given a number with improper number of digits
         return false;
     }
 
--- Java/CreditCardValidator/src/CreditCardValidatorTest.java
+++ Java/CreditCardValidator/src/CreditCardValidatorTest.java
@@ -20,6 +20,12 @@ class CreditCardValidatorTest {
     }
 
     @Test
+    void validateCardReturnsFalseWhenGivenATenDigitNumber() {
+        boolean result = CreditCardValidator.validateCard(1234567890L);
+        assertFalse(result);
+    }
+
+    @Test
     void reverseNumber() {
         ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(5, 4, 3, 2, 1, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1));
         assertEquals(expected, CreditCardValidator.reverseDigitArray(123456789012345L));
